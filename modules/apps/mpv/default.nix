82e097df5965a5b6790e04a61504ec2c4c4026a9
Mageas <gpg@mageas.net>
2023-06-08 10:32:16 +0200

    add mpv

---
+++ modules/apps/mpv/default.nix
@@ -0,0 +1,15 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.internal;
+let
+  cfg = config.plusultra.apps.mpv;
+in
+{
+  options.plusultra.apps.mpv = with types; {
+    enable = mkBoolOpt false "Whether or not to enable mpv.";
+  };
+
+  config =
+    mkIf cfg.enable { environment.systemPackages = with pkgs; [ mpv ]; };
+}
