0d3b1bfe210f7ebeaccef39b4871b270638b442b
Mageas <gpg@mageas.net>
2023-06-08 18:47:42 +0200

    add qbittorrent

---
+++ modules/apps/qbittorrent/default.nix
@@ -0,0 +1,14 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.internal;
+let cfg = config.plusultra.apps.qbittorrent;
+in
+{
+  options.plusultra.apps.qbittorrent = with types; {
+    enable = mkBoolOpt false "Whether or not to enable qbittorrent.";
+  };
+
+  config =
+    mkIf cfg.enable { environment.systemPackages = with pkgs; [ qbittorrent ]; };
+}
