f576caf91cd9da46f44c5a3311d776fd62edf596
Mageas <gpg@mageas.net>
2023-06-08 11:03:53 +0200

    add jellyfin

---
+++ modules/services/jellyfin/default.nix
@@ -0,0 +1,18 @@
+{ options, config, pkgs, lib, ... }:
+
+with lib;
+with lib.internal;
+let cfg = config.plusultra.services.jellyfin;
+in
+{
+  options.plusultra.services.jellyfin = with types; {
+    enable = mkBoolOpt false "Whether or not to configure jellyfin.";
+  };
+
+  config = mkIf cfg.enable { 
+    services.jellyfin = {
+      enable = true; 
+      group = "media";
+    };
+  };
+}
