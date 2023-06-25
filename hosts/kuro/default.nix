6ea5754a4a909c89f942fd0dce7e0c68e98b6569
Mageas <gpg@mageas.net>
2023-06-25 22:51:50 +0200

    update kuro/default

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -12,12 +12,12 @@
     };
   };
 
-  services.xserver = {
-    enable = true;
+  # services.xserver = {
+  #   enable = true;
 
-    displayManager.defaultSession = "none+dwm";
-    displayManager.lightdm.enable = true;
-  };
+  #   displayManager.defaultSession = "none+dwm";
+  #   displayManager.lightdm.enable = true;
+  # };
 
 
   # services.xserver.windowManager.session = pkgs.lib.singleton { 
@@ -31,26 +31,26 @@
   
 
 
-  # services.xserver = {
-  #   enable = true;
+  services.xserver = {
+    enable = true;
 
-  #   # Touchpad
-  #   libinput.enable = true;
+    # Touchpad
+    libinput.enable = true;
 
-  #   # desktopManager.gnome.enable = true;
-  #   displayManager.defaultSession = "none+dwm";
-  #   displayManager.lightdm = {
-  #     enable = true;
-  #     greeters.gtk = {
-  #       theme.name = "Sweet-mars";
-  #       cursorTheme = {
-  #         package = pkgs.bibata-cursors;
-  #         name = "Bibata_Amber";
-  #         size = 32;
-  #       };
-  #     };
-  #   };
-  # };
+    # desktopManager.gnome.enable = true;
+    displayManager.defaultSession = "none+dwm";
+    displayManager.lightdm = {
+      enable = true;
+      greeters.gtk = {
+        theme.name = "Sweet-mars";
+        # cursorTheme = {
+        #   package = pkgs.bibata-cursors;
+        #   name = "Bibata_Amber";
+        #   size = 32;
+        # };
+      };
+    };
+  };
 
 
 
