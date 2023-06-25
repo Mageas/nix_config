70a0e6d9b7a1ebfe3b80a5e33596d3cc4b3f57d4
Mageas <gpg@mageas.net>
2023-06-25 10:00:08 +0200

    update kuro/hardware

--- hosts/kuro/hardware-configuration.nix
+++ hosts/kuro/hardware-configuration.nix
@@ -4,7 +4,7 @@
   imports = [ "${modulesPath}/installer/scan/not-detected.nix" ];
 
   boot = {
-    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "virtio_pci" "sr_mod" "virtio_blk" ];
     initrd.kernelModules = [];
     extraModulePackages = [];
     kernelModules = [ "kvm-amd" ];
@@ -20,44 +20,22 @@
     kernel.sysctl."net.ipv4.icmp_echo_ignore_broadcasts" = 1;
   };
 
-  # Modules
-  modules.hardware = {
-    audio.enable = true;
-    ergodox.enable = true;
-    fs = {
-      enable = true;
-      ssd.enable = true;
-    };
-    nvidia.enable = true;
-    sensors.enable = true;
-  };
+  # # Modules
+  # modules.hardware = {
+  #   audio.enable = true;
+  #   ergodox.enable = true;
+  #   fs = {
+  #     enable = true;
+  #     ssd.enable = true;
+  #   };
+  #   nvidia.enable = true;
+  #   sensors.enable = true;
+  # };
 
   # CPU
   nix.settings.max-jobs = lib.mkDefault 16;
   powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.amd.updateMicrocode = true;
-
-  # Displays
-  services.xserver = {
-    # This must be done manually to ensure my screen spaces are arranged exactly
-    # as I need them to be *and* the correct monitor is "primary". Using
-    # xrandrHeads does not work.
-    monitorSection = ''
-      VendorName  "Unknown"
-      ModelName   "DELL U2515H"
-      HorizSync   30.0 - 113.0
-      VertRefresh 56.0 - 86.0
-      Option      "DPMS"
-    '';
-    screenSection = ''
-      Option "metamodes" "HDMI-0: nvidia-auto-select +1920+0, DVI-I-1: nvidia-auto-select +0+180, DVI-D-0: nvidia-auto-select +4480+180"
-      Option "SLI" "Off"
-      Option "MultiGPU" "Off"
-      Option "BaseMosaic" "off"
-      Option "Stereo" "0"
-      Option "nvidiaXineramaInfoOrder" "DFP-1"
-    '';
-  };
 
   # Storage
   fileSystems = {
@@ -67,30 +45,9 @@
       options = [ "noatime" ];
     };
     "/boot" = {
-      device = "/dev/disk/by-label/BOOT";
+      device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
-    "/home" = {
-      device = "/dev/disk/by-label/home";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-    "/usr/drive" = {
-      device = "kiiro:/volume1/homes/hlissner/Drive";
-      fsType = "nfs";
-      options = [
-        "nofail" "noauto" "noatime" "x-systemd.automount" "x-systemd.idle-timeout=5min"
-        "nodev" "nosuid" "noexec"
-      ];
-    };
-    "/usr/store" = {
-      device = "/dev/disk/by-label/store";
-      fsType = "ext4";
-      options = [
-        "noauto" "noatime" "x-systemd.automount" "x-systemd.idle-timeout=5min"
-        "nodev" "nosuid" "noexec"
-      ];
-    };
-  };
+ };
   swapDevices = [];
 }
