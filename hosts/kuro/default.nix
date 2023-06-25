466f635e500a8977e7ff806897dec87a31ebee85
Mageas <gpg@mageas.net>
2023-06-25 22:57:37 +0200

    update kuro/default

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -20,15 +20,34 @@
   # };
 
 
-  # services.xserver.windowManager.session = pkgs.lib.singleton { 
-  #   name = "dwm";
-  #   start =
-  #     ''
-  #       dwm &
-  #       waitPID=$!
-  #     '';
-  # };
-  
+  services.xserver.windowManager.session = pkgs.lib.singleton { 
+    name = "dwm";
+    start =
+      ''
+        if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+          eval $(dbus-launch --exit-with-session --sh-syntax)
+        fi
+        systemctl --user import-environment DISPLAY XAUTHORITY
+
+        if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+                dbus-update-activation-environment DISPLAY XAUTHORITY
+        fi
+
+        dwm &
+        waitPID=$!
+      '';
+  };
+
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd dwm";
+        user = "mageas";
+      };
+    };
+  };
 
 
   services.xserver = {
@@ -38,19 +57,31 @@
     libinput.enable = true;
 
     # desktopManager.gnome.enable = true;
-    displayManager.defaultSession = "none+dwm";
-    displayManager.lightdm = {
-      enable = true;
-      greeters.gtk = {
-        theme.name = "Sweet-mars";
-        # cursorTheme = {
-        #   package = pkgs.bibata-cursors;
-        #   name = "Bibata_Amber";
-        #   size = 32;
-        # };
-      };
-    };
+    displayManager.lightdm.enable = false;
   };
+  
+
+
+  # services.xserver = {
+  #   enable = true;
+
+  #   # Touchpad
+  #   libinput.enable = true;
+
+  #   # desktopManager.gnome.enable = true;
+  #   displayManager.defaultSession = "none+dwm";
+  #   displayManager.lightdm = {
+  #     enable = true;
+  #     greeters.gtk = {
+  #       theme.name = "Sweet-mars";
+  #       # cursorTheme = {
+  #       #   package = pkgs.bibata-cursors;
+  #       #   name = "Bibata_Amber";
+  #       #   size = 32;
+  #       # };
+  #     };
+  #   };
+  # };
 
 
 
