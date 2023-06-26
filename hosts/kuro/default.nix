b65c223357e88b110651157d439a319138679aba
Mageas <gpg@mageas.net>
2023-06-26 17:01:52 +0200

    update kuro/default remove wallpaper

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -22,7 +22,6 @@
     # desktopManager.gnome.enable = true;
     displayManager.lightdm = {
       enable = true;
-      background = "${config.dotfiles.dir}/hosts/kuro/wallpaper.jpg";
       greeters.gtk = {
         enable = true;
         theme = {
@@ -39,10 +38,7 @@
     
     displayManager.defaultSession = "none+dwm";
   };
-
-  # environment.etc."wallpaper.jpg".source = "./wallpaper.jpg";
-  # environment.etc."wallpaper.jpg".source = "${pkgs.lightdm}/share/wallpaper.jpg";
-
+  
 
   ## Local config
   programs.ssh.startAgent = true;
