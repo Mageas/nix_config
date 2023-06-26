3d7c0233da051894f28aa6f3272eb00ccbb6e16b
Mageas <gpg@mageas.net>
2023-06-26 06:58:42 +0200

    update kuro/default

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -12,41 +12,11 @@
     };
   };
 
-  # services.xserver = {
-  #   enable = true;
-
-  #   displayManager.defaultSession = "none+dwm";
-  #   displayManager.lightdm.enable = true;
-  # };
-
-
-  # services.xserver.windowManager.session = pkgs.lib.singleton { 
-  #   name = "dwm";
-  #   start =
-  #     ''
-  #       if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-  #         eval $(dbus-launch --exit-with-session --sh-syntax)
-  #       fi
-  #       systemctl --user import-environment DISPLAY XAUTHORITY
-
-  #       if command -v dbus-update-activation-environment >/dev/null 2>&1; then
-  #               dbus-update-activation-environment DISPLAY XAUTHORITY
-  #       fi
-
-  #       dwm &
-  #       waitPID=$!
-  #     '';
-  # };
-
-
-  services.greetd = {
+  services.xserver = {
     enable = true;
-    settings = {
-      default_session = {
-        command = "dbus-run-session ${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd dwm";
-        user = "mageas";
-      };
-    };
+
+    displayManager.defaultSession = "none+dwm";
+    displayManager.ly.enable = true;
   };
 
 
@@ -61,30 +31,6 @@
     # desktopManager.gnome.enable = true;
     displayManager.lightdm.enable = false;
   };
-  
-
-
-  # services.xserver = {
-  #   enable = true;
-
-  #   # Touchpad
-  #   libinput.enable = true;
-
-  #   # desktopManager.gnome.enable = true;
-  #   displayManager.defaultSession = "none+dwm";
-  #   displayManager.lightdm = {
-  #     enable = true;
-  #     greeters.gtk = {
-  #       theme.name = "Sweet-mars";
-  #       # cursorTheme = {
-  #       #   package = pkgs.bibata-cursors;
-  #       #   name = "Bibata_Amber";
-  #       #   size = 32;
-  #       # };
-  #     };
-  #   };
-  # };
-
 
 
   ## Local config
