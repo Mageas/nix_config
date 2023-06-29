43d9b1492e555110d0d045420df771fcbef94940
Mageas <gpg@mageas.net>
2023-06-29 23:58:45 +0200

    update

--- modules/desktop/addons/autostart/default.nix
+++ modules/desktop/addons/autostart/default.nix
@@ -4,39 +4,6 @@ with lib;
 with lib.internal;
 let 
   cfg = config.plusultra.desktop.addons.autostart;
-  # lightdmAutostart = pkgs.stdenvNoCC.mkDerivation {
-  #   name = "lightdm-autostart";
-  #   src = ./lightdm-autostart;
-  #   phases = [ "build" "install" ];
-  #   buildInputs = [];
-  #   buildPhase = ''
-  #     echo "Building lightdm-autostart"
-  #   '';
-  #   installPhase = ''
-  #     mkdir -p $out/share/lightdm-autostart
-  #     cp -r $src $out/share/lightdm-autostart/
-  #   '';
-  # };
-
-  # lightdmAutostart = pkgs.runCommandNoCC "lightdm-autostart"
-  #   { } ''
-  #     local target="$out/share"
-  #     mkdir -p "$target"
-
-  #     cp ${./lightdm-autostart} "$target/lightdm-autostart"
-  #   '';
-
-  # lightdmAutostart = pkgs.stdenvNoCC.mkDerivation {
-  #   name = "lightdm-autostart";
-  #   src = ./.;
-  #   phases = [ "unpackPhase" "installPhase" ];
-  #   installPhase = ''
-  #     local target="/run/current-system/sw/share"
-
-  #     mkdir -p $target
-  #     cp $src/lightdm-autostart $target
-  #   '';
-  # };
 in
 {
   options.plusultra.desktop.addons.autostart = with types; {
@@ -45,8 +12,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    # environment.systemPackages = with pkgs; [ lightdmAutostart ];
-    # environment.etc."lightdm/lightdm-autostart".source = ./lightdm-autostart;
     plusultra.home.file.".xprofile".source = ./xprofile;
   };
 }
