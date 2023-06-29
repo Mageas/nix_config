6780cf43670c2753ab85ce30c7138725ab0b71d4
Mageas <gpg@mageas.net>
2023-06-29 11:32:44 +0200

    add alacritty

---
+++ modules/desktop/addons/alacritty/default.nix
@@ -0,0 +1,15 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.internal;
+let cfg = config.plusultra.desktop.addons.alacritty;
+in
+{
+  options.plusultra.desktop.addons.alacritty = with types; {
+    enable = mkBoolOpt false "Whether to enable Alacritty.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ alacritty ];
+  };
+}
