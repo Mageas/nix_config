01cba31931cf35ad7c7769923a29ecfb6db27932
Mageas <gpg@mageas.net>
2023-06-29 22:03:52 +0200

    update

--- modules/desktop/addons/autostart/default.nix
+++ modules/desktop/addons/autostart/default.nix
@@ -4,19 +4,26 @@ with lib;
 with lib.internal;
 let 
   cfg = config.plusultra.desktop.addons.autostart;
-  lightdmAutostart = pkgs.stdenvNoCC.mkDerivation {
-    name = "lightdm-autostart";
-    src = ./lightdm-autostart;
-    phases = [ "build" "install" ];
-    buildInputs = [];
-    buildPhase = ''
-      echo "Building lightdm-autostart"
+  # lightdmAutostart = pkgs.stdenvNoCC.mkDerivation {
+  #   name = "lightdm-autostart";
+  #   src = ./lightdm-autostart;
+  #   phases = [ "build" "install" ];
+  #   buildInputs = [];
+  #   buildPhase = ''
+  #     echo "Building lightdm-autostart"
+  #   '';
+  #   installPhase = ''
+  #     mkdir -p $out/share/lightdm-autostart
+  #     cp -r $src $out/share/lightdm-autostart/
+  #   '';
+  # };
+  lightdmAutostart = pkgs.runCommandNoCC "lightdm-autostart"
+    { } ''
+      local target="$out/share"
+      mkdir -p "$target"
+
+      cp ./lightdm-autostart "$target/lightdm-autostart"
     '';
-    installPhase = ''
-      mkdir -p $out/share/lightdm-autostart
-      cp -r $src $out/share/lightdm-autostart/
-    '';
-  };
 in
 {
   options.plusultra.desktop.addons.autostart = with types; {
