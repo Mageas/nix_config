ef6ebd5b05f19a4f86a52e874d2c65aa33c7038e
Mageas <gpg@mageas.net>
2023-06-29 15:30:18 +0200

    fix ca.desrt.dconf error for home-manager

--- systems/x86_64-linux/jasper/default.nix
+++ systems/x86_64-linux/jasper/default.nix
@@ -19,6 +19,7 @@ with lib.internal;
     };
   };
 
+  # Fix ca.desrt.dconf error for home-manager (https://nix-community.github.io/home-manager/index.html#_why_do_i_get_an_error_message_about_literal_ca_desrt_dconf_literal_or_literal_dconf_service_literal)
   programs.dconf.enable = true;
 
   # plusultra.home.extraOptions = {
