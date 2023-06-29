2f01098c89c6bb5ba349d67d7941a321c67701e7
Mageas <gpg@mageas.net>
2023-06-29 16:31:55 +0200

    update

--- modules/desktop/dwm/default.nix
+++ modules/desktop/dwm/default.nix
@@ -96,7 +96,7 @@ in
         # Type = "simple";
         ExecStartPre="${pkgs.coreutils}/bin/sleep 2";
         ExecStart = ''
-          /home/${config.plusultra.user.name}/.config/dwm/autostart
+          /home/${config.plusultra.user.name}/.config/dwm/autostart &
         '';
       };
       wantedBy = [ "default.target" ];
