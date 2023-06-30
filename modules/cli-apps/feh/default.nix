6d972ccbd197ff0bcc5e96f06e3f0fcc6731b449
Mageas <gpg@mageas.net>
2023-06-30 20:03:19 +0200

    add feh

---
+++ modules/cli-apps/feh/default.nix
@@ -0,0 +1,18 @@
+inputs@{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.internal;
+let
+  cfg = config.plusultra.cli-apps.feh;
+in
+{
+  options.plusultra.cli-apps.feh = with types; {
+    enable = mkBoolOpt false "Whether or not to enable feh.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      feh
+    ];
+  };
+}
