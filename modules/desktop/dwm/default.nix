e3a1e74d929b15c8b85bd6946e30c45dd36b47bf
Mageas <gpg@mageas.net>
2023-06-30 19:56:47 +0200

    udpate

--- modules/desktop/dwm/default.nix
+++ modules/desktop/dwm/default.nix
@@ -22,7 +22,7 @@ in
       sxhkd = enabled;
       wallpapers = {
         enable = true;
-        wallpapers.name = "nord-rainbow-dark-nix";
+        name = "nord-rainbow-dark-nix";
         # wallpapers = pkgs.plusultra.wallpapers.nord-rainbow-dark-nix;
       };
       xdg-portal = enabled;
