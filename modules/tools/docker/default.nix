5994ee66065ad84573eba0ef4f5ab1affdc5d2bd
Mageas <gpg@mageas.net>
2023-07-01 17:41:20 +0200

    update docker

--- modules/tools/docker/default.nix
+++ modules/tools/docker/default.nix
@@ -11,7 +11,13 @@ in
   };
 
   config = mkIf cfg.enable {
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
 
     plusultra.user.extraGroups = [ "docker" ];
   };
