3d2331cda6151ef930d2aacdcad2b1452d98383f
Mageas <gpg@mageas.net>
2023-07-01 21:49:31 +0200

    update dunst

--- modules/services/dunst/default.nix
+++ modules/services/dunst/default.nix
@@ -11,7 +11,7 @@ in
   };
 
   config = mkIf cfg.enable { 
-    environment.systemPackages = with pkgs; [ dunst ];
+    environment.systemPackages = with pkgs; [ dunst libnotify ];
 
     systemd = {
       user.services.dunst = {
