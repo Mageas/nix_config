b8d4c598621e1aacbcb2256ebb74995591204e3b
Mageas <gpg@mageas.net>
2023-07-01 20:24:45 +0200

    update gammastep

--- modules/apps/gammastep/default.nix
+++ modules/apps/gammastep/default.nix
@@ -13,6 +13,22 @@ in
   config = mkIf cfg.enable { 
     environment.systemPackages = with pkgs; [ gammastep ];
 
+    systemd = {
+      user.services.gammastep = {
+        description = "gammastep";
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+            Type = "simple";
+            ExecStart = "${pkgs.gammastep}";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+          };
+      };
+    };
+
     plusultra.home.configFile."gammastep/config.ini".source = ./config.ini;
   };
 }
