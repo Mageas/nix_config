7fe1813f94e6fa513537172b0a83e25159e04305
Mageas <gpg@mageas.net>
2023-07-02 15:16:48 +0200

    update

--- systems/x86_64-linux/jasper/default.nix
+++ systems/x86_64-linux/jasper/default.nix
@@ -53,6 +53,12 @@ with lib.internal;
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+
+    nvidiaPatches = false;
+    xwayland = {
+      enable = true;
+      hidpi = true;
+    };
   };
 
   # Fix ca.desrt.dconf error for home-manager (https://nix-community.github.io/home-manager/index.html#_why_do_i_get_an_error_message_about_literal_ca_desrt_dconf_literal_or_literal_dconf_service_literal)
