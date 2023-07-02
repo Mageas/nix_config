28778bad6711d8431e9d7426d8b7b89b8f1f1e3e
Mageas <gpg@mageas.net>
2023-07-02 15:10:29 +0200

    update

--- systems/x86_64-linux/jasper/default.nix
+++ systems/x86_64-linux/jasper/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, nixos-hardware, hyprland, ... }:
+{ pkgs, lib, nixos-hardware, inputs, ... }:
 
 with lib;
 with lib.internal;
@@ -52,7 +52,7 @@ with lib.internal;
   # programs.hyprland.enable = true;
   programs.hyprland = {
     enable = true;
-    package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   };
 
   # Fix ca.desrt.dconf error for home-manager (https://nix-community.github.io/home-manager/index.html#_why_do_i_get_an_error_message_about_literal_ca_desrt_dconf_literal_or_literal_dconf_service_literal)
