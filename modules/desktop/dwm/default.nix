951dfd883bc8f1d2277dc53e9a9cb83ce47fa2f9
Mageas <gpg@mageas.net>
2023-07-03 10:55:43 +0200

    remove dwm hack

--- modules/desktop/dwm/default.nix
+++ modules/desktop/dwm/default.nix
@@ -35,16 +35,6 @@ in
             rev = "712d6639ff8e863560328131bbb92b248dc9cde7";
           }; 
           patches = [
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/alwayscenter.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/bar.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/config.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/fakefullscreen.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/focusonclick.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/fullgaps.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/movestack.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/nomonocleborder.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/picom.diff)
-            # (builtins.fetchurl https://raw.githubusercontent.com/Mageas/dwm/main/patches/warp.diff)
             (super.fetchpatch {
               url = "https://raw.githubusercontent.com/Mageas/dwm/main/patches/alwayscenter.diff";
               sha256 = "sha256-Z0+WNfb4aniWML3/P+46BC7A36OpbfG0prL7ZA7jX54=";
