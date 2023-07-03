c3ac2c934025c72f6c8e37e904392c6253e1a52a
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2023-07-03 18:38:12 +0200

    test

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Plus Ultra";
 
   inputs = {
-    # NixPkgs (nixos-23.05)
+    # NixPkgs (nixos-22.11)
     nixpkgs.url =
       "github:nixos/nixpkgs/nixos-23.05";
 
@@ -10,13 +10,22 @@
     unstable.url =
       "github:nixos/nixpkgs/nixos-unstable";
 
-    # Home Manager (release-23.05)
+    # Home Manager (release-22.05)
     home-manager.url =
       "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # macOS Support (master)
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     # Hardware Configuration
     nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    # Generate System Images
+    nixos-generators.url =
+      "github:nix-community/nixos-generators";
+    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
     # Snowfall Lib
     snowfall-lib.url = "github:snowfallorg/lib/dev";
@@ -25,7 +34,11 @@
     # Snowfall Flake
     flake.url = "github:snowfallorg/flake";
     flake.inputs.nixpkgs.follows = "unstable";
-    # flake.inputs.snowfall-lib.follows = "snowfall-lib";
+
+    # Comma
+    comma.url =
+      "github:nix-community/comma";
+    comma.inputs.nixpkgs.follows = "unstable";
 
     # System Deployment
     deploy-rs.url = "github:serokell/deploy-rs";
@@ -38,6 +51,56 @@
     # Neovim
     neovim.url = "github:jakehamilton/neovim";
     neovim.inputs.nixpkgs.follows = "unstable";
+
+    # Binary Cache
+    attic = {
+      url = "github:zhaofengli/attic";
+      # @FIXME(jakehamilton): A specific version of Rust is needed right now or
+      # the build fails. Re-enable this after some time has passed.
+
+      inputs.nixpkgs.follows = "unstable";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+
+    # Vault Integration
+    vault-service = {
+      url = "github:DeterminateSystems/nixos-vault-service";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Flake Hygiene
+    flake-checker = {
+      url = "github:DeterminateSystems/flake-checker";
+      inputs.nixpkgs.follows = "unstable";
+    };
+
+    # Discord Replugged
+    replugged.url = "github:LunNova/replugged-nix-flake";
+    replugged.inputs.nixpkgs.follows = "unstable";
+
+    # Discord Replugged plugins / themes
+    discord-tweaks = {
+      url = "github:NurMarvin/discord-tweaks";
+      flake = false;
+    };
+    discord-nord-theme = {
+      url = "github:DapperCore/NordCord";
+      flake = false;
+    };
+
+    # Cows!
+    cowsay = {
+      url = "github:snowfallorg/cowsay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
+
+    # Backup management
+    icehouse = {
+      url = "github:snowfallorg/icehouse";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
 
     # Yubikey Guide
     yubikey-guide = {
@@ -55,6 +118,37 @@
       url = "github:suchipi/Bibata_Cursor";
       flake = false;
     };
+
+    # Hosted Sites
+    lasersandfeelings = {
+      url = "github:jakehamilton/lasersandfeelings";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
+    scrumfish = {
+      url = "github:jakehamilton/scrumfi.sh";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
+    retrospectacle = {
+      url = "github:jakehamilton/retrospectacle.app";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
+    jakehamilton-website = {
+      url = "github:jakehamilton/jakehamilton.dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
+    noop-ai-website = {
+      url = "github:noopai/noop.ai";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
+    sokoban-app-website = {
+      url = "https://github.com/jakehamilton/sokoban.app/releases/download/v1/sokoban.app.tar.gz";
+      flake = false;
+    };
   };
 
   outputs = inputs:
@@ -67,16 +161,29 @@
     lib.mkFlake {
       package-namespace = "plusultra";
 
-      channels-config.allowUnfree = true;
+      channels-config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          # @FIXME(jakehamilton): This is a workaround for 22.11 and can
+          # be removed once NixPkgs is upgraded to 23.05.
+          "electron-20.3.11"
+          "nodejs-16.20.0"
+        ];
+      };
 
       overlays = with inputs; [
         neovim.overlay
         flake.overlay
+        cowsay.overlay
+        icehouse.overlay
+        attic.overlays.default
       ];
 
       systems.modules = with inputs; [
         home-manager.nixosModules.home-manager
         nix-ld.nixosModules.nix-ld
+        # attic.nixosModules.atticd
+        vault-service.nixosModules.nixos-vault-service
       ];
 
       systems.hosts.jasper.modules = with inputs; [
