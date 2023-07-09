7f10434cc9246395fdc33dab68c4e12cb53e2388
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2023-07-09 23:34:39 +0200

    udpate

--- packages/joshuto-open/default.nix
+++ packages/joshuto-open/default.nix
@@ -8,14 +8,14 @@ writeShellApplication
   name = "joshuto-open";
   checkPhase = "";
   text = ''
-    cwd_file='/tmp/joshuto-cwd'
-    env joshuto --output-file '$cwd_file' --path '$(pwd)' $@
+    cwd_file="/tmp/joshuto-cwd"
+    env joshuto --output-file "$cwd_file" --path "$(pwd)" $@
 
     echo "joshuto" > /tmp/test
 
-    if [ -e '$cwd_file' ]; then
-        joshuto_cwd=$(cat '$cwd_file')
-        rm '$cwd_file' &>/dev/null && joshuto_cwd=$joshuto_cwd alacritty -e 'cd $joshuto_cwd; clear; zsh -i'
-    fi
+    # if [ -e "$cwd_file" ]; then
+    #     joshuto_cwd=$(cat "$cwd_file")
+    #     rm "$cwd_file" &>/dev/null && joshuto_cwd=$joshuto_cwd alacritty -e 'cd "$joshuto_cwd"; clear; zsh -i'
+    # fi
   '';
 } 
