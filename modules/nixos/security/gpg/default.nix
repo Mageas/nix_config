b52ba25a4e3cbc8b971125250217a4fda3cdda00
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2023-07-10 12:47:31 +0200

    fix: gpg pinentry

--- modules/nixos/security/gpg/default.nix
+++ modules/nixos/security/gpg/default.nix
@@ -15,41 +15,41 @@ let
     max-cache-ttl-ssh 86400
   '';
 
-  guide = "${inputs.yubikey-guide}/README.md";
+  # guide = "${inputs.yubikey-guide}/README.md";
 
-  theme = pkgs.fetchFromGitHub {
-    owner = "jez";
-    repo = "pandoc-markdown-css-theme";
-    rev = "019a4829242937761949274916022e9861ed0627";
-    sha256 = "1h48yqffpaz437f3c9hfryf23r95rr319lrb3y79kxpxbc9hihxb";
-  };
+  # theme = pkgs.fetchFromGitHub {
+  #   owner = "jez";
+  #   repo = "pandoc-markdown-css-theme";
+  #   rev = "019a4829242937761949274916022e9861ed0627";
+  #   sha256 = "1h48yqffpaz437f3c9hfryf23r95rr319lrb3y79kxpxbc9hihxb";
+  # };
 
-  guideHTML = pkgs.runCommand "yubikey-guide" { } ''
-    ${pkgs.pandoc}/bin/pandoc \
-      --standalone \
-      --metadata title="Yubikey Guide" \
-      --from markdown \
-      --to html5+smart \
-      --toc \
-      --template ${theme}/template.html5 \
-      --css ${theme}/docs/css/theme.css \
-      --css ${theme}/docs/css/skylighting-solarized-theme.css \
-      -o $out \
-      ${guide}
-  '';
+  # guideHTML = pkgs.runCommand "yubikey-guide" { } ''
+  #   ${pkgs.pandoc}/bin/pandoc \
+  #     --standalone \
+  #     --metadata title="Yubikey Guide" \
+  #     --from markdown \
+  #     --to html5+smart \
+  #     --toc \
+  #     --template ${theme}/template.html5 \
+  #     --css ${theme}/docs/css/theme.css \
+  #     --css ${theme}/docs/css/skylighting-solarized-theme.css \
+  #     -o $out \
+  #     ${guide}
+  # '';
 
-  guideDesktopItem = pkgs.makeDesktopItem {
-    name = "yubikey-guide";
-    desktopName = "Yubikey Guide";
-    genericName = "View Yubikey Guide in a web browser";
-    exec = "${pkgs.xdg-utils}/bin/xdg-open ${guideHTML}";
-    icon = ./yubico-icon.svg;
-    categories = [ "System" ];
-  };
+  # guideDesktopItem = pkgs.makeDesktopItem {
+  #   name = "yubikey-guide";
+  #   desktopName = "Yubikey Guide";
+  #   genericName = "View Yubikey Guide in a web browser";
+  #   exec = "${pkgs.xdg-utils}/bin/xdg-open ${guideHTML}";
+  #   icon = ./yubico-icon.svg;
+  #   categories = [ "System" ];
+  # };
 
-  reload-yubikey = pkgs.writeShellScriptBin "reload-yubikey" ''
-    ${pkgs.gnupg}/bin/gpg-connect-agent "scd serialno" "learn --force" /bye
-  '';
+  # reload-yubikey = pkgs.writeShellScriptBin "reload-yubikey" ''
+  #   ${pkgs.gnupg}/bin/gpg-connect-agent "scd serialno" "learn --force" /bye
+  # '';
 in
 {
   options.plusultra.security.gpg = with types; {
@@ -58,8 +58,8 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.pcscd.enable = true;
-    services.udev.packages = with pkgs; [ yubikey-personalization ];
+    # services.pcscd.enable = true;
+    # services.udev.packages = with pkgs; [ yubikey-personalization ];
 
     # @NOTE(jakehamilton): This should already have been added by programs.gpg, but
     # keeping it here for now just in case.
@@ -83,9 +83,10 @@ in
       gnupg
       pinentry-curses
       pinentry-qt
+      pinentry-gtk2
       paperkey
-      guideDesktopItem
-      reload-yubikey
+      # guideDesktopItem
+      # reload-yubikey
     ];
 
     programs = {
@@ -95,14 +96,15 @@ in
         enable = true;
         enableSSHSupport = true;
         enableExtraSocket = true;
-        pinentryFlavor = "gnome3";
+        pinentryFlavor = "gtk2";
+        # pinentryFlavor = "gnome3";
       };
     };
 
     plusultra = {
       home.file = {
-        ".gnupg/yubikey-guide.md".source = guide;
-        ".gnupg/yubikey-guide.html".source = guideHTML;
+        # ".gnupg/yubikey-guide.md".source = guide;
+        # ".gnupg/yubikey-guide.html".source = guideHTML;
 
         ".gnupg/gpg.conf".source = gpgConf;
         ".gnupg/gpg-agent.conf".text = gpgAgentConf;
